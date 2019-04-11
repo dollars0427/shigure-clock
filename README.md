a7736e2e74a1edfa74062d6f95543774a1dd65c9
Sardo <sardo@pixelactionstudio.com>
2019-04-11 14:37:34 +0800

    Add icon and README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Shigure Clock
+
+只是個到整點就會報時的時雨語音鬧鐘。
+
+## Credit
+Icon Designed by Freepik from www.flaticon.com
--- main.js
+++ main.js
@@ -1,19 +1,50 @@
 const electron = require('electron');
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const app = electron.app;
+const Menu = electron.Menu;
+const Tray = electron.Tray;
+const nativeImage = electron.nativeImage;
+const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path')
 const url = require('url')
 
-let mainWindow
+let mainWindow;
+
+function createTray(){
+  let appIcon = null
+  const iconPath = path.join(__dirname, 'clock-mini.png');
+  const icon = nativeImage.createFromPath(iconPath);
+  const contextMenu = Menu.buildFromTemplate([{
+    label: 'Show Shigure Clock',
+    click() {
+      mainWindow.show();
+    }
+  },
+  {
+    label: 'Hide',
+    click() {
+      mainWindow.hide();
+    }
+  },
+  {
+    label: 'Quit',
+    click() {
+      mainWindow.close()
+    }
+  }
+]);
+appIcon = new Tray(icon);
+appIcon.setToolTip('Shigure Clock')
+appIcon.setContextMenu(contextMenu);
+}
 
 function createWindow () {
   mainWindow = new BrowserWindow({
-      width: 200,
-      height: 80,
-      transparent: true,
-      frame: false,
-      alwaysOnTop:true
+    width: 200,
+    height: 80,
+    transparent: true,
+    frame: false,
+    alwaysOnTop:true,
   })
 
   mainWindow.loadURL(url.format({
@@ -26,15 +57,15 @@ function createWindow () {
 
   mainWindow.on('closed', function () {
     mainWindow = null
+    app.quit();
   })
+  createTray();
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  app.quit();
 })
 
 app.on('activate', function () {
